b1821c0cb5e29d13213f7ec448993a5bf9dc8c97
ismael094 <ismaelpitufo@gmail.com>
2020-01-19 11:34:33 +0000

    Third test: one deaf rat on each side. Red and green test

--- src/test/java/DeafRatsCounter_.java
+++ src/test/java/DeafRatsCounter_.java
@@ -3,6 +3,10 @@ import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class DeafRatsCounter_ {
+
+    public static final String LEFT_DEAF_RAT = "O~";
+    public static final String RIGHT_DEAF_RAT = "~O";
+
     @Test
     public void an_empty_string_should_return_zero_deaf_rats() {
         assertThat(countDeafRats("")).isEqualTo(0);
@@ -18,16 +22,25 @@ public class DeafRatsCounter_ {
         assertThat(countDeafRats("P ~O ~O")).isEqualTo(2);
     }
 
+    @Test
+    public void a_pied_piper_and_one_deaf_rats_on_each_side_should_return_two_deaf_rats() {
+        assertThat(countDeafRats("O~ P ~O")).isEqualTo(2);
+    }
+
     private int countDeafRats(String ratsOfHamelin) {
         if (ratsOfHamelin.length() == 0)
             return 0;
-        return deafRats(ratsOfHamelin.substring(ratsOfHamelin.indexOf("P")));
+        return countDeafRats(ratsOfHamelin.substring(0,ratsOfHamelin.indexOf("P")), LEFT_DEAF_RAT) + countDeafRats(ratsOfHamelin.substring(ratsOfHamelin.indexOf("P")+1).trim(), RIGHT_DEAF_RAT);
     }
 
-    private int deafRats(String str) {
-        if (str.contains("~O"))
-            return 1 + deafRats(str.substring(str.indexOf("~O")+2));
+    private int countDeafRats(String str, String deafRat) {
+        if (str.contains(deafRat))
+            return 1 + countDeafRats(str.substring(str.indexOf(deafRat)+2),deafRat);
+
+
         return 0;
     }
+
+
 
 }
